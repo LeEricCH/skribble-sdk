dcb43eabfa02735e1c29edfca4a248c7ab9f8504
Eric <ec@webix.ch>
2024-09-29 16:06:51 +0200

    chore(py-sdk): release version 0.5

--- apps/py-sdk/setup.py
+++ apps/py-sdk/setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='skribble-sdk',
-    version='0.4',
+    version='0.5',
     packages=find_packages(),
     install_requires=[
         'requests',
